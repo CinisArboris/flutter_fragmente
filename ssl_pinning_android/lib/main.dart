6010b0594ee029c10341e4903b62fc693c6e5fde
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-08-16 18:29:07 -0400

    update : stable : ssl pinning

--- ssl_pinning_android/lib/main.dart
+++ ssl_pinning_android/lib/main.dart
@@ -1,125 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:http_certificate_pinning/http_certificate_pinning.dart';
+import 'package:dio/dio.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldMessengerState> _messengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+  String? _webContent;
 
-  void _incrementCounter() {
+  _fetchAndSetWebContent(String url) async {
+    var dio = Dio();
+    var response = await dio.get(url);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _webContent = response.data.toString().substring(0, 500);
     });
   }
 
+  check(String url, String fingerprint, SHA sha, int timeout) async {
+    List<String> allowedShA1FingerprintList = [];
+    allowedShA1FingerprintList.add(fingerprint);
+
+    try {
+      String checkMsg = await HttpCertificatePinning.check(
+          serverURL: url,
+          sha: sha,
+          allowedSHAFingerprints: allowedShA1FingerprintList,
+          timeout: timeout);
+
+      if (!mounted) return;
+
+      _messengerKey.currentState?.showSnackBar(
+        SnackBar(
+          content: Text(checkMsg),
+          duration: const Duration(seconds: 1),
+          backgroundColor: Colors.green,
+        ),
+      );
+      _fetchAndSetWebContent(url);
+    } catch (e) {
+      _messengerKey.currentState?.showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+          duration: const Duration(seconds: 1),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
+  void submit() {
+    if (_formKey.currentState?.validate() == true) {
+      _formKey.currentState?.save();
+
+      check(
+        'https://www.nasa.gov/',
+        '06:B5:E2:55:E5:DC:CF:7A:13:CF:8B:D9:24:77:AF:CC:93:6A:A7:3D:68:66:37:A3:FD:DB:2A:72:17:86:8E:BC',
+        SHA.SHA256,
+        60,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      scaffoldMessengerKey: _messengerKey,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Ssl Pinning Plugin'),
+        ),
+        body: Builder(
+          builder: (BuildContext context) {
+            return Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                key: _formKey,
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(top: 20.0),
+                      child: ElevatedButton(
+                        onPressed: () => submit(),
+                        child: const Text(
+                          'Check',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    if (_webContent != null) ...[
+                      Divider(),
+                      Text('Web Content:'),
+                      Text(_webContent!),
+                    ]
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
