1d0385dfa29b96231248f21c131c56db311b5bbe
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2024-01-09 10:09:28 -0400

    Update MainActivity.java

--- alerta_caida/android/app/src/main/java/com/cinisarboris/example/u02/alerta_caida/MainActivity.java
+++ alerta_caida/android/app/src/main/java/com/cinisarboris/example/u02/alerta_caida/MainActivity.java
@@ -1,6 +1,26 @@
 package com.cinisarboris.example.u02.alerta_caida;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "com.cinisarboris.example/audio";
+
+    @Override
+    public void configureFlutterEngine(FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            if (call.method.equals("setVolumeToMax")) {
+                                AudioHelper audioHelper = new AudioHelper(getApplicationContext());
+                                audioHelper.setVolumeToMax();
+                                result.success(null);
+                            } else {
+                                result.notImplemented();
+                            }
+                        }
+                );
+    }
 }
