46f5ca99ef2f0668a09c1630d0455eee894f00b3
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2024-01-18 16:03:53 -0400

    Update image_uploader.dart

--- fstorage/lib/firebase/image_uploader.dart
+++ fstorage/lib/firebase/image_uploader.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 class ImageUploader {
   final ImagePicker _picker = ImagePicker();
@@ -21,10 +23,34 @@ class ImageUploader {
 
     try {
       final ref = _storage.ref().child(folderName).child(fileName);
-      await ref.putFile(localImage);
-      return localImage;
-    } catch (e) {
+      UploadTask uploadTask = ref.putFile(localImage);
+
+      TaskSnapshot snapshot = await uploadTask;
+
+      if (snapshot.state == TaskState.success) {
+        // Aquí puedes agregar cualquier procesamiento adicional después de una subida exitosa
+        final downloadUrl = await snapshot.ref.getDownloadURL();
+        if (kDebugMode) {
+          print('Carga exitosa. URL: $downloadUrl');
+        }
+        // Por ejemplo, podrías devolver la URL de descarga en lugar del archivo local
+        return localImage; // o return downloadUrl;
+      } else {
+        if (kDebugMode) {
+          print('Error en la subida. Estado: ${snapshot.state}');
+        }
+        return null;
+      }
+    } on FirebaseException catch (e) {
+      if (kDebugMode) {
+        print('Error de Firebase: ${e.code}, ${e.message}');
+      }
       rethrow;
+    } catch (e) {
+      if (kDebugMode) {
+        print('Exception: ${e.toString()}');
+      }
+      throw 'Error al subir imagen: ${e.toString()}';
     }
   }
 }
