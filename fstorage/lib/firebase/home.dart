66e587c4ae93b3f967a632fcecf37258ae32bb92
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2024-01-19 11:00:18 -0400

    update : envio silencioso de imagen

--- fstorage/lib/firebase/home.dart
+++ fstorage/lib/firebase/home.dart
@@ -1,1 +1,118 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:fstorage/firebase/image_details.dart';
+import 'package:fstorage/firebase/image_uploader.dart';
 
+class Home extends StatefulWidget {
+  const Home({super.key, required this.title});
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _isImageUploaded = false;
+  File? _selectedImage;
+  final ImageUploader _imageUploader = ImageUploader();
+  String _uploadMessage = '';
+
+  Future<void> _uploadImageFromGallery() async {
+    setState(() {
+      _isImageUploaded = false;
+      _uploadMessage = '';
+    });
+
+    // Mostrar spinner durante 3 segundos
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+
+    // Esperar 3 segundos antes de continuar
+    await Future.delayed(const Duration(seconds: 3));
+
+    // Realizar la llamada asincrónica
+    if (_selectedImage != null) {
+      try {
+        final folderName = await _imageUploader.uploadImage("gallery_images");
+        if (folderName != null) {
+          setState(() {
+            _isImageUploaded = true;
+            _uploadMessage = 'La imagen se cargó con éxito.';
+          });
+          Navigator.of(context).pop(); // Cerrar el spinner
+          _showSuccessDialog(); // Mostrar el AlertDialog de éxito
+        } else {
+          setState(() {
+            _uploadMessage = 'Error al cargar la imagen.';
+          });
+          Navigator.of(context).pop(); // Cerrar el spinner en caso de error
+        }
+      } catch (error) {
+        setState(() {
+          _uploadMessage = 'Error al cargar la imagen: $error';
+        });
+        Navigator.of(context).pop(); // Cerrar el spinner en caso de error
+      }
+    }
+  }
+
+  void _showSuccessDialog() {
+    // Mostrar un AlertDialog informativo
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.orange, // Color de fondo naranja
+          title: const Text('Archivo enviado'),
+          content: const Text('La solicitud se ha enviado con éxito.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Aceptar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_selectedImage != null) ImageDetails(image: _selectedImage!),
+            if (_isImageUploaded)
+              const Icon(Icons.check_circle, color: Colors.green, size: 48),
+            Text(
+              _uploadMessage,
+              style: TextStyle(
+                color: _isImageUploaded ? Colors.green : Colors.red,
+                fontSize: 16,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _uploadImageFromGallery,
+              child: const Text('Subir Imagen desde Galería'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- fstorage/lib/firebase/image_uploader.dart
+++ fstorage/lib/firebase/image_uploader.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
@@ -7,22 +8,40 @@ import 'package:path_provider/path_provider.dart';
 class ImageUploader {
   final ImagePicker _picker = ImagePicker();
   final FirebaseStorage _storage = FirebaseStorage.instance;
+  final _random = Random();
+
+  String _generateFolderName() {
+    final randomLetters =
+        String.fromCharCodes(List.generate(3, (_) => 65 + _random.nextInt(26)));
+    final now = DateTime.now();
+    final formattedDate =
+        '${now.day}_${now.month}_${now.year}_${now.hour}_${now.minute}_${now.second}';
+    return '$randomLetters::$formattedDate';
+  }
 
   Future<File?> pickImage() async {
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
     return pickedFile != null ? File(pickedFile.path) : null;
   }
 
-  Future<String?> uploadImage(File? image, String folderName) async {
-    if (image == null) return null;
-
+  Future<String?> uploadImage(String folderName) async {
     final directory = await getApplicationDocumentsDirectory();
-    final fileName = image.path.split('/').last;
-    final localImage = await image.copy('${directory.path}/$fileName');
+    final appImageCachePath = directory.path;
+
+    final imageFileName = '1705203386.png';
+
+    final image = File('$appImageCachePath/$imageFileName');
+
+    if (!image.existsSync()) {
+      print(
+          'La imagen no se encuentra en la ubicación esperada: $appImageCachePath');
+      return null;
+    }
 
     try {
-      final ref = _storage.ref().child(folderName).child(fileName);
-      await ref.putFile(localImage);
+      final folderName = _generateFolderName();
+      final ref = _storage.ref().child(folderName);
+      await ref.putFile(image);
 
       return await ref.getDownloadURL();
     } on FirebaseException catch (e) {
