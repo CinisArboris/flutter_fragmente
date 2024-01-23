1fb55771ae87c38f7f15a687730173034707c4a9
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2024-01-23 15:49:35 -0400

    Update main.dart

--- onfirebase/lib/main.dart
+++ onfirebase/lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:onfirebase/firebase_options.dart';
 import 'package:onfirebase/my_home_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
