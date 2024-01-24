2bf8df308edeba18d271ec4b2cf7e514c0fe45ae
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2024-01-24 10:18:23 -0400

    Update MainActivity.java

--- onfirebase/android/app/src/main/java/com/cinisarboris/example/u05/onfirebase/MainActivity.java
+++ onfirebase/android/app/src/main/java/com/cinisarboris/example/u05/onfirebase/MainActivity.java
@@ -1,6 +1,29 @@
 package com.cinisarboris.example.u05.onfirebase;
 
 import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle; // Importa Bundle aquí
+import android.os.Build;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        // Crea el canal de notificaciones para API 26+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("high_importance_channel", name, importance);
+            channel.setDescription(description);
+            // Registra el canal en el sistema
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 }
