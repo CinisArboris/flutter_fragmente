2814b66c4818beda13d3b3c3c1e01f3290842347
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2024-01-24 09:40:15 -0400

    Update fcm_service.dart
    - mostrar datos del mensaje en primer plano

--- onfirebase/lib/fcm_service.dart
+++ onfirebase/lib/fcm_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 
 class FCMService {
   final FirebaseMessaging _messaging = FirebaseMessaging.instance;
@@ -6,14 +7,35 @@ class FCMService {
   Future<void> initFCM() async {
     // Solicita permisos para iOS
     NotificationSettings settings = await _messaging.requestPermission();
-    print('User granted permission: ${settings.authorizationStatus}');
+    if (kDebugMode) {
+      print('User granted permission: ${settings.authorizationStatus}');
+    }
 
     // Suscribirse a un tópico
     await _messaging.subscribeToTopic('bec_alerta_general');
 
+    // // Manejo de mensajes en primer plano
+    // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    //   if (kDebugMode) {
+    //     print('Received a message: ${message.messageId}');
+    //   }
+    //   // Aquí puedes manejar el mensaje como desees.
+    // });
+
+    // En la clase FCMService
+
     // Manejo de mensajes en primer plano
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Received a message: ${message.messageId}');
+      if (kDebugMode) {
+        print('Received a message: ${message.messageId}');
+        if (message.notification != null) {
+          print('Message title: ${message.notification!.title}');
+          print('Message body: ${message.notification!.body}');
+        }
+        if (message.data.isNotEmpty) {
+          print('Message data: ${message.data}');
+        }
+      }
       // Aquí puedes manejar el mensaje como desees.
     });
   }
