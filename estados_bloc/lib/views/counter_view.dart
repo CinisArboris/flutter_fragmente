f2a35fc6c9fdade2c8da2b8a3073fcac6054aa1d
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2024-04-04 16:16:08 -0400

    update : base counter

---
+++ estados_bloc/lib/views/counter_view.dart
@@ -0,0 +1,35 @@
+import 'package:estados_bloc/cubits/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterView extends StatefulWidget {
+  const CounterView({
+    super.key,
+    required this.title,
+  });
+  final String title;
+
+  @override
+  State<CounterView> createState() => _CounterViewState();
+}
+
+class _CounterViewState extends State<CounterView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text('$state');
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read<CounterCubit>().increment(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
