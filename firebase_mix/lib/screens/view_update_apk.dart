f9d5752b65fc555e61cbcd9f482582f59340197a
CinisArboris <eyver.evm@gmail.com>
2024-08-11 15:17:26 -0400

    Update view_update_apk.dart

--- firebase_mix/lib/screens/view_update_apk.dart
+++ firebase_mix/lib/screens/view_update_apk.dart
@@ -26,52 +26,91 @@ class ViewUpdateApkState extends State<ViewUpdateApk> {
     installer = GestorDeActualizaciones(widget.apkUrl);
 
     if (widget.apkUrl.isEmpty) {
-      // Si la URL es vacía, mostrar un mensaje de error
       setState(() {
         errorMessage =
             'Error conectando a Firebase. Favor de actualizar las URLs o contactar con soporte.';
       });
     } else {
-      installer?.descargarEInstalarActualizacion(
-        onBytesDownloaded: (mbDownloaded) {
-          _downloadingWidgetKey.currentState
-              ?.updateBytesDownloaded(mbDownloaded);
-        },
-      ).catchError((error) {
-        // Manejar el error y mostrar un mensaje al usuario
-        setState(() {
-          errorMessage =
-              'Error durante la descarga o instalación de la APK: $error';
-        });
-      });
+      _startDownload();
     }
+  }
+
+  void _startDownload() {
+    installer?.descargarEInstalarActualizacion(
+      onBytesDownloaded: (mbDownloaded) {
+        _downloadingWidgetKey.currentState?.updateBytesDownloaded(mbDownloaded);
+      },
+    ).catchError((error) {
+      setState(() {
+        errorMessage =
+            'Error durante la descarga o instalación de la APK: $error';
+      });
+    });
+  }
+
+  Widget _buildErrorWidget() {
+    return DioErrorWidget(error: errorMessage);
+  }
+
+  Widget _buildDownloadingWidget() {
+    return DioDownloadingWidget(
+      key: _downloadingWidgetKey,
+    );
+  }
+
+  Widget _buildSuccessWidget() {
+    return const DioSuccessWidget();
+  }
+
+  Widget _buildFutureBuilder() {
+    return FutureBuilder(
+      future: installer?.descargarEInstalarActualizacion(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return _buildDownloadingWidget();
+        } else if (snapshot.hasError) {
+          return DioErrorWidget(error: snapshot.error.toString());
+        } else {
+          return _buildSuccessWidget();
+        }
+      },
+    );
+  }
+
+  Widget _buildContainer() {
+    return Container(
+      constraints: const BoxConstraints(
+        maxWidth: 300,
+        maxHeight: 400,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Colors.grey.shade400,
+          width: 2,
+        ),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.all(16), // Reducir el padding interno
+      child: errorMessage != null ? _buildErrorWidget() : _buildFutureBuilder(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Actualización de la Aplicación'),
-      ),
+      backgroundColor: Colors.grey[200], // Fondo gris claro
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child: errorMessage != null
-              ? DioErrorWidget(error: errorMessage)
-              : FutureBuilder(
-                  future: installer?.descargarEInstalarActualizacion(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return DioDownloadingWidget(
-                        key: _downloadingWidgetKey,
-                      );
-                    } else if (snapshot.hasError) {
-                      return DioErrorWidget(error: snapshot.error.toString());
-                    } else {
-                      return const DioSuccessWidget();
-                    }
-                  },
-                ),
+          child: _buildContainer(),
         ),
       ),
     );
