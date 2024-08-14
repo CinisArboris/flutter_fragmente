ddd7efbe434c5b37ac8eb4dffa04759b47ae70a0
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2024-08-14 15:50:01 -0400

    gui : rename files

---
+++ firebase_mix/lib/widgets/main_apk_dialog_update/dialog_title.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Widget buildDialogTitle() {
+  _logWithSeparator('Construyendo título del diálogo');
+  return const Row(
+    children: [
+      Icon(Icons.system_update, color: Colors.white),
+      SizedBox(width: 10),
+      Text(
+        'Actualización disponible',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
+        ),
+      ),
+    ],
+  );
+}
+
+void _logWithSeparator(String message) {
+  debugPrint(':::: UpdateDialog - $message');
+}
